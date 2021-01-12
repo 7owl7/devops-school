d79043d7c21037d1eeddbe2ef15c0f62080a02c2
7owl7 <61232888+7owl7@users.noreply.github.com>
2021-01-12 12:43:46 +0300

    Update main.py
    Added writing user input content to log.md

--- scripts/main.py
+++ scripts/main.py
@@ -3,8 +3,11 @@ from datetime import datetime
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+with open('/variable.txt', 'r') as file:
+    user_input = file.read().replace('\n', '')
 
 with open('log.md', 'w') as f:
-    f.write(f'# {timestamp}')
-    
+    f.write(f'# {user_input}  {timestamp} ')
+
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
+
